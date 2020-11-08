a3d1304bd8dcb8c5e97cb012bbd9d4a452b53f86
Mike Vondrak <mvondrak@gmail.com>
2020-11-08 11:17:23 -0700

    capitalization error

--- client/src/app/store/active-font-set/selectors/active-font-set.selectors.ts
+++ client/src/app/store/active-font-set/selectors/active-font-set.selectors.ts
@@ -1,7 +1,7 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { FontSet } from '../../../models/font-set.model';
 import { AppState } from '../../state';
-import { ActiveFontSetState } from '../active-font-Set.state';
+import { ActiveFontSetState } from '../active-font-set.state';
 import { featureKey } from '../feature-key';
 
 export const selectFeatureActiveFontSet = createFeatureSelector<AppState, ActiveFontSetState>(featureKey);
