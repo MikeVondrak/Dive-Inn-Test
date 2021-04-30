4abecd34a589427fd6ae3892f2adb3e04446f998
rick haag <rick_haag@hotmail.com>
2021-04-29 23:21:16 -0600

    working on fontsetlist dropdown
    Co-authored-by: mvondrak@gmail.com <mvondrak@gmail.com>

--- client/src/app/pages/demo/demo.page.component.ts
+++ client/src/app/pages/demo/demo.page.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { FontInstance } from 'src/app/models/font-instance.model';
+import { FontSetListView } from 'src/app/models/font-set.model';
 import { UiFont } from 'src/app/models/ui-font.model';
+import { FontSetManagerService } from 'src/app/services/font-set-manager/font-set-manager.service';
+import { DropdownItem } from 'src/app/shared/components/form-controls/dropdown/dropdown.component';
+import { setActiveFontSetById } from 'src/app/store/active-font-set/actions/active-font-set.actions';
 import { getUiActiveFontInstance } from 'src/app/store/app.selectors';
 import { AppState } from 'src/app/store/state';
 
@@ -19,15 +23,23 @@ export class DemoPageComponent implements OnInit {
   public textStyle = '';
 
   public activeFontInstance$: Observable<FontInstance>;
+  public fontSetList$: Observable<FontSetListView[]> = this.fontSetManagerService.fontSetsListView$;
 
   constructor(
     private pageLoadingService: PageLoadingService,
-    private store$: Store<AppState>
+    private store$: Store<AppState>,
+    private fontSetManagerService: FontSetManagerService,
   ) {
   }
 
   ngOnInit(): void {
     this.activeFontInstance$ = this.store$.select(getUiActiveFontInstance);
+  }
+
+  selectedFontSetChange($event: DropdownItem) {
+    const fontSet: FontSetListView = $event as FontSetListView;
+    // TODO: can make this a separate action / store state if we don't want changes in the Demo page affecting Select page
+    this.store$.dispatch(setActiveFontSetById({fontSetId: fontSet.setId}))
   }
 
 }
